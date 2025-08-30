c5d663713dfeceeb57fff9944b0661bf9e37088d
Lepotototor <victor.flament@epita.fr>
2025-08-30 06:50:11 +0200

    [MODIFY] Wallpapers

--- i3/start.sh
+++ i3/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mv ~/.config/hypr ~/.config/hyprv
-mv ~/.config/kitty ~/.config/kittyv
+# mv ~/.config/hypr ~/.config/hyprv
+# mv ~/.config/kitty ~/.config/kittyv
 
 bg=0
 CHANGE_BG() 
@@ -12,18 +12,23 @@ CHANGE_BG()
 	i3lock -p win -i "$HOME/.config/i3/img/loadings/loading$bg.png"
 }
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
-nix-channel --add https://github.com/nixos/nixpkgs/archive/refs/tags/24.05.tar.gz nixpkgs
-nix-channel --update
+#nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
+#nix-channel --add https://github.com/nixos/nixpkgs/archive/refs/tags/24.05.tar.gz nixpkgs
+#nix-channel --update
 
-export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
-nix-shell '<home-manager>' -A install
+# export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
+# nix-shell '<home-manager>' -A install
 
-unzip ~/.config/i3/CascadiaCode.zip -d ~/.local/share/fonts
-fc-cache -f -v
+#unzip ~/.config/i3/CascadiaCode.zip -d ~/.local/share/fonts
+# fc-cache -f -v
 
 picom &
 polybar &
+
+# rm -rf ~/.config/hypr
+# rm -rf ~/.config/kitty
+# cp -r ~/.config/hyprv ~/.config/hypr
+# cp -r ~/.config/kittyv ~/.config/kitty
 
 feh --bg-fill "$HOME/.config/i3/Wallpapers/fairy.png"
 xset r rate 250
